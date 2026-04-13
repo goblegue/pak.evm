53ee6c78792c6281957a245364a88e38e20270f5
Abdullah <bajwaabd576@gmail.com>
2026-04-13 23:24:48 +0500

    Database Lead: Initialized 7 collections with unique indexes

---
+++ online_voting_app/databasemanager.h
@@ -0,0 +1,21 @@
+#ifndef DATABASEMANAGER_H
+#define DATABASEMANAGER_H
+
+#include<mongocxx/client.hpp>
+#include<mongocxx/instance.hpp>
+#include<mongocxx/database.hpp>
+
+class DatabaseManager
+{
+public:
+    static DatabaseManager & getInstance();
+    //Collections Setup
+    void setupSchema();
+private:
+    DatabaseManager();
+    mongocxx::instance inst{};
+    mongocxx::client client;
+    mongocxx::database db;
+};
+
+#endif // DATABASEMANAGER_H
