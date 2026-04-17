359e5ab2ee4495e74ef81eb4e7d682297c248b49
goblegue <101870464+goblegue@users.noreply.github.com>
2026-04-17 09:42:06 +0500

    bug fixes and rejection logic added to election,admin,candidates

--- online_voting_app/states.h
+++
@@ -1,8 +0,0 @@
-#ifndef STATES_H
-#define STATES_H
-
-enum class ElectionState { Draft, Published, VotingOpen, VotingClosed, ResultsAnnounced };
-
-enum class ApprovalStatus { Pending, Approved, Rejected };
-
-#endif // STATES_H
