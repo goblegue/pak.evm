ce16184732da3c63f8e052e285894fa9623c7085
goblegue <101870464+goblegue@users.noreply.github.com>
2026-04-20 10:12:20 +0500

    added candidate Controller

---
+++ online_voting_app/src/candidate/candidateController.h
@@ -0,0 +1,33 @@
+#ifndef CANDIDATE_CONTROLLER_H
+#define CANDIDATE_CONTROLLER_H
+
+#include "candidate.h"
+#include "../user_management/admin/admin.h"
+#include "../election/election.h"
+
+class CandidateController
+{
+    ICandidateRepository *m_candidateRepo;
+    IAdminRepository *m_adminRepo;
+    IElectionRepository *m_electionRepo;
+    CandidateController();
+    ~CandidateController();
+
+public:
+    CandidateController(const CandidateController &) = delete;
+    void operator=(const CandidateController &) = delete;
+
+    static CandidateController &getInstance();
+    void injectRepositories(ICandidateRepository *candidateRepo, IAdminRepository *adminRepo, IElectionRepository *electionRepo);
+
+    bool createCandidate(Candidate &candidate);
+    bool requestCandidateStatusChange(const QString &candidateCnic, const QString &adminCnic, ApprovalStatus status);
+
+    Candidate *getCandidatesByElection(const QString &electionId, int &candidatesSize, bool isAdminRequesting= false);
+
+    Candidate *getCandidatesByElectionAndStatus(const QString &electionId, ApprovalStatus status, int &candidatesSize);
+    
+    QString getCandidatesJsonByElection(const QString &electionId);
+};
+
+#endif // CANDIDATE_CONTROLLER_H
--- online_voting_app/src/election/electionController.cpp
+++ online_voting_app/src/election/electionController.cpp
@@ -1,22 +1,26 @@
 #include "electionController.h"
+#include <optional>
 
-ElectionManager::ElectionManager() : m_electionRepo(nullptr), m_adminRepo(nullptr) {}
+const int ELECTION_APPROVAL_THRESHOLD = 2; // More than 50% of admins must approve
+const int ELECTION_REJECTION_THRESHOLD = 3; // More than 33% of admins
 
-ElectionManager::~ElectionManager() {}
+ElectionController::ElectionController() : m_electionRepo(nullptr), m_adminRepo(nullptr) {}
 
-ElectionManager &ElectionManager::getInstance()
+ElectionController::~ElectionController() {}
+
+ElectionController &ElectionController::getInstance()
 {
-    static ElectionManager instance;
+    static ElectionController instance;
     return instance;
 }
 
-void ElectionManager::injectRepositories(IElectionRepository *electionRepo, IAdminRepository *adminRepo)
+void ElectionController::injectRepositories(IElectionRepository *electionRepo, IAdminRepository *adminRepo)
 {
     m_electionRepo = electionRepo;
     m_adminRepo = adminRepo;
 }
 
-bool ElectionManager::createElection(const Election &election)
+bool ElectionController::createElection(const Election &election)
 {
     if (!m_electionRepo)
     {
@@ -28,7 +32,7 @@ bool ElectionManager::createElection(const Election &election)
     {
         if (existingElections[i].getId() == election.getId() ||
             (existingElections[i].getTitle() == election.getTitle() &&
-            existingElections[i].getStatus() != ElectionState::ResultsAnnounced))
+             existingElections[i].getStatus() != ElectionState::ResultsAnnounced))
         {
             delete[] existingElections;
             return false; // Election with same ID or title which results are not announced already exists
@@ -38,23 +42,26 @@ bool ElectionManager::createElection(const Election &election)
     return m_electionRepo->insertElection(election);
 }
 
-bool ElectionManager::requestElectionStatusChange(const QString &electionId, const QString &adminId, ApprovalStatus status)
+bool ElectionController::requestElectionStatusChange(const QString &electionId, const QString &adminId, ApprovalStatus status)
 {
     if (!m_electionRepo || !m_adminRepo)
     {
         return false; // Repositories not injected
     }
     auto electionOpt = m_electionRepo->getElectionById(electionId);
-    if (!electionOpt.has_value()){
+    if (!electionOpt.has_value())
+    {
         return false; // Election not found
     }
     Election election = electionOpt.value();
     auto adminOpt = m_adminRepo->getAdminByCnic(adminId);
-    if (!adminOpt.has_value()){
-        return false; // Admin not found   
+    if (!adminOpt.has_value())
+    {
+        return false; // Admin not found
     }
     Admin admin = adminOpt.value();
-    if (admin.getStatus() != ApprovalStatus::Approved){
+    if (admin.getStatus() != ApprovalStatus::Approved)
+    {
         return false; // Admin not approved
     }
     m_electionRepo->addStatusChangeRequest(electionId, adminId, status);
@@ -63,16 +70,18 @@ bool ElectionManager::requestElectionStatusChange(const QString &electionId, con
     int rejectedCount = election.getStatusCount(ApprovalStatus::Rejected);
 
     int totalAdmins = m_adminRepo->getAdminCount();
-    if (approvedCount >= (totalAdmins / 2) ){
-        return m_electionRepo->updateElectionState(electionId, ElectionState::Published)); 
+    if (approvedCount > (totalAdmins / ELECTION_APPROVAL_THRESHOLD))
+    {
+        return m_electionRepo->updateElectionState(electionId, ElectionState::Published);
     }
-    if (rejectedCount >= (totalAdmins / 2) ){
-        return m_electionRepo->updateElectionState(electionId, ElectionState::Rejected)); 
+    if (rejectedCount > (totalAdmins / ELECTION_REJECTION_THRESHOLD))
+    {
+        return m_electionRepo->updateElectionState(electionId, ElectionState::Rejected);
     }
     return true;
 }
 
-Election *ElectionManager::getAllElections(int &electionsSize)
+Election *ElectionController::getAllElections(int &electionsSize)
 {
     if (!m_electionRepo)
     {
@@ -82,7 +91,7 @@ Election *ElectionManager::getAllElections(int &electionsSize)
     return m_electionRepo->getAllElections(electionsSize);
 }
 
-Election *ElectionManager::getElectionsByStatus(ElectionState status, int &electionsSize)
+Election *ElectionController::getElectionsByStatus(ElectionState status, int &electionsSize)
 {
     if (!m_electionRepo)
     {
@@ -104,7 +113,29 @@ Election *ElectionManager::getElectionsByStatus(ElectionState status, int &elect
     return filteredElections;
 }
 
-// std::optional<Election> ElectionManager::getElectionById(const QString &id)
+Election *ElectionController::getElectionsForUser(int &electionsSize)
+{
+    if (!m_electionRepo)
+    {
+        electionsSize = 0;
+        return nullptr; // Repository not injected
+    }
+    Election *allElections = m_electionRepo->getAllElections(electionsSize);
+    Election *filteredElections = new Election[electionsSize];
+    int count = 0;
+    for (int i = 0; i < electionsSize; ++i)
+    {
+        if (allElections[i].getStatus() >= ElectionState::Published)
+        {
+            filteredElections[count++] = allElections[i];
+        }
+    }
+    delete[] allElections;
+    electionsSize = count;
+    return filteredElections;
+}
+
+// std::optional<Election> ElectionController::getElectionById(const QString &id)
 // {
 //     if (!m_electionRepo)
 //     {
