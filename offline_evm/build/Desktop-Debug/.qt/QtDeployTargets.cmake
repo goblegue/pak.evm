43925f1999a148fdf4ab7ff11bbf03c644bdc506
goblegue <101870464+goblegue@users.noreply.github.com>
2026-04-25 21:41:40 +0500

    offline evm system added

---
+++ offline_evm/build/Desktop-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_offline_evm_FILE /home/hamza/Documents/FAST CY/SEMESTER 2/oop/project/pak.evm/offline_evm/build/Desktop-Debug/offline_evm)
+set(__QT_DEPLOY_TARGET_offline_evm_TYPE EXECUTABLE)
---
+++ offline_evm/build/Desktop-Debug/.qt/deploy_offline_evm_f7f6267495.cmake
@@ -0,0 +1,8 @@
+include("/home/hamza/Documents/FAST CY/SEMESTER 2/oop/project/pak.evm/offline_evm/build/Desktop-Debug/.qt/QtDeploySupport.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/offline_evm-plugins.cmake" OPTIONAL)
+set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
+
+qt6_deploy_runtime_dependencies(
+    EXECUTABLE "/home/hamza/Documents/FAST CY/SEMESTER 2/oop/project/pak.evm/offline_evm/build/Desktop-Debug/offline_evm"
+    GENERATE_QT_CONF
+)
