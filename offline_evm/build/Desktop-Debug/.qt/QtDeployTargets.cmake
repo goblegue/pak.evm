22b8a030486f21c36f33872ea56fa75a33d87816
goblegue <101870464+goblegue@users.noreply.github.com>
2026-04-26 11:50:42 +0500

    cmake config added

--- offline_evm/build/Desktop-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_offline_evm_FILE /home/hamza/Documents/FAST CY/SEMESTER 2/oop/project/pak.evm/offline_evm/build/Desktop-Debug/offline_evm)
-set(__QT_DEPLOY_TARGET_offline_evm_TYPE EXECUTABLE)
