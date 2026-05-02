62b2c8453b05bc572372d113c7fe4795f6b68377
povahmad-hub <am7862760@gmail.com>
2026-05-02 10:21:38 +0500

    created offline system startup setup window

--- offline_evm/src/main.cpp
+++ offline_evm/src/main.cpp
@@ -1,11 +1,21 @@
 #include "views/mainwindow.h"
+#include  "views/OfflineSetupWizard.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
     MainWindow w;
-    w.show();
-    return QCoreApplication::exec();
+    OfflineSetupWizard *wizard = new OfflineSetupWizard();
+
+    // When wizard finishes, show main window and delete the wizard
+    QObject::connect(wizard, &OfflineSetupWizard::setupFinished, [&]() {
+        w.show();
+        wizard->deleteLater(); // Safely delete the wizard object
+    });
+
+    wizard->show();
+    return a.exec();
 }
